60f07645c32480c521bd9aaa5f73dff27ea34669
HBSocrates <ayang426@gmail.com>
2026-02-20 18:29:52 -0800

    Added settings to math game

--- vite-project/src/MathGameApp.jsx
+++ vite-project/src/MathGameApp.jsx
@@ -14,12 +14,26 @@ function MathGameApp() {
   const [answer, setAnswer] = useState(0);
   const [response, setResponse] = useState("");
   const [score, setScore] = useState(0);
-  const [min, setMin] = useState(1);
-  const [max, setMax] = useState(12);
+  const [min, setMin] = useState(localStorage.getItem('min') ? localStorage.getItem('min') : -12);
+  const [max, setMax] = useState(localStorage.getItem('max') ? localStorage.getItem('max') : 12);
 
-  const keyPressed = (event) => {
+  const answerKeyPressed = (event) => {
     if (event.key === "Enter") {
       validateSolution();
+    }
+  }
+  
+  const minKeyPressed = (event) => {
+    if (event.key === "Enter") {
+      localStorage.setItem('min', event.target.value);
+      setMin(event.target.value);
+    }
+  }
+
+  const maxKeyPressed = (event) => {
+    if (event.key === "Enter") {
+      localStorage.setItem('max', event.target.value);
+      setMax(event.target.value);
     }
   }
 
@@ -73,15 +87,37 @@ function MathGameApp() {
     <>
       <div>
         <h1> Maths! </h1>
-        <p>Score: {score}</p>
-        <p>High Score: {highScore}</p>
+        <p>Score: {score} High Score: {highScore}</p>
         <p>{randNum1} {randOperand.name} {randNum2} =</p>
-        <p>{solution}</p>
         <label>
-          <input name="answer" type="number" defaultValue="0" onChange={e => setAnswer(e.target.value)} onKeyDown={keyPressed}/>
+          <input name="answer" type="number" defaultValue="0" onChange={e => setAnswer(e.target.value)} onKeyDown={answerKeyPressed}/>
           <button name="submit" onClick={() => {validateSolution()}}> Submit </button>
         </label>
         <p>{response}</p>
+      </div>
+      <div>
+          <h2>Settings</h2>
+          <label>
+            Min:
+            <input name="min" type="number" defaultValue={localStorage.getItem('min') ? localStorage.getItem('min') : -12} onKeyDown={minKeyPressed}/> <br></br>
+            Max:
+            <input name="max" type="number" defaultValue={localStorage.getItem('max') ? localStorage.getItem('max') : 12} onKeyDown={maxKeyPressed}/> <br></br>
+            <button name="submit" onClick={() => {
+              localStorage.setItem('min', document.getElementsByName('min')[0].value);
+              localStorage.setItem('max', document.getElementsByName('max')[0].value);
+              setMin(document.getElementsByName('min')[0].value);
+              setMax(document.getElementsByName('max')[0].value);
+            }}> Submit </button>
+            <br></br>
+            <button name="reset" onClick={() => {
+              localStorage.setItem('min', -12);
+              localStorage.setItem('max', 12);
+              setMin(-12);
+              setMax(12);
+              document.getElementsByName('min')[0].value = -12;
+              document.getElementsByName('max')[0].value = 12;
+            }}> Reset </button>
+          </label>
       </div>
     </>
   )
