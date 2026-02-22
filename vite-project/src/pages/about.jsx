7ed39ae60718145fffc6a12e63632a5ec55bc0ed
HBSocrates <ayang426@gmail.com>
2026-02-21 19:52:14 -0800

    cleaned up files, updated about me with basic contact info

--- vite-project/src/pages/about.jsx
+++ vite-project/src/pages/about.jsx
@@ -7,6 +7,11 @@ const About = () => {
             <p>
                 I am a computer programmer, specializing in full-stack development. I have experience in a variety of programming languages and frameworks, and I am always eager to learn new technologies. I am passionate about creating efficient and user-friendly applications that solve real-world problems.
             </p>
+            <p>
+                Contact info:
+                Email: <a href="mailto:ayang426@gmail.com">ayang426@gmail.com</a>
+                Phone: 559-709-2023
+            </p>
         </div>
     );
 };
