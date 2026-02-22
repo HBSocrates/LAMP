b36b1c68c9c2913ef058316672f5e2f3abc6484e
HBSocrates <ayang426@gmail.com>
2026-02-21 20:06:16 -0800

    changed navbar color

--- vite-project/src/components/NavBar/navBarElements.jsx
+++ vite-project/src/components/NavBar/navBarElements.jsx
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-    background: #63d471;
+    background: #18023d;
     height: 85px;
     display: flex;
     justify-content: space-between;
