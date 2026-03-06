21eeaba2ecbf1771807a1f1b19e102a7ef4ed6a4
HBSocrates <ayang426@gmail.com>
2026-03-05 17:50:19 -0800

    added NavBtn, NavBtnLink to imports for future use

--- vite-project/src/components/NavBar/NavBar.jsx
+++ vite-project/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, NavLink, Bars, NavMenu } from "./NavBarElements";
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from "./NavBarElements";
 
 const Navbar = () => {
     return (
