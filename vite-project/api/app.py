0564d8cc86d1c08ac86c919cd74e26b254e4a7c4
HBSocrates <ayang426@gmail.com>
2026-09-18 11:53:38 -0700

    Changed hard coded user to string from form

--- vite-project/api/app.py
+++ vite-project/api/app.py
@@ -127,7 +127,7 @@ def get_data():
 def login():
     loginUser = request.form['username']
     passString = request.form['password']
-    selectedUsers = db.session.execute(db.select(users_template).filter_by(username='test')).scalars().all()
+    selectedUsers = db.session.execute(db.select(users_template).filter_by(username=loginUser)).scalars().all()
     print('Received login attempt for user:', loginUser, file=sys.stderr)
 
     hash = db.session.execute(func.crypt(passString, func.gen_salt('md5'))).scalars().all()
