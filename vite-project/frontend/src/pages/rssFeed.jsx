7c7e0602e7b60fb29dced61c65194988bc2fe462
HBSocrates <ayang426@gmail.com>
2026-09-18 16:21:54 -0700

    Update rssFeed.jsx

--- vite-project/frontend/src/pages/rssFeed.jsx
+++ vite-project/frontend/src/pages/rssFeed.jsx
@@ -65,7 +65,6 @@ const RSSFeed = () => {
         } catch (error) {
             console.error('Error fetching feeds:', error);
         }
-        setCurrentUrl(rssUrl)
     }, [parseFeedMessage]);
 
     useEffect(() => {
